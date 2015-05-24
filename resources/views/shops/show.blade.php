780d2c84f699c63aa34ed55a90e88a1ea71b9411
Kanitkorn Sujautra <k.sujautra@gmail.com>
2015-05-24 08:09:56 +0700

    add shop show view

---
+++ resources/views/shops/show.blade.php
@@ -0,0 +1,16 @@
+@extends('app')
+
+@section('content')
+  <h2>{{ $shop->name }}</h2>
+
+  <p>Name : {{ $shop->name }}</p>
+  <p>Slug : {{ $shop->slug }}</p>
+  <p>Tel : {{ $shop->tel }}</p>
+  <p>Rate : {{ $shop->rate }}</p>
+  <p>Description : {{ $shop->description }}</p>
+  <p>Place : {{ $shop->place }}</p>
+  <p>Created At : {{ $shop->created_at }}</p>
+  <p>Updated At : {{ $shop->updated_at }}</p>
+
+  {!! link_to_route('shops.index', 'Back to Shops') !!}
+@endsection
